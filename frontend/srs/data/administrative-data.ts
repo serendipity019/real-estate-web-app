7bafbfb71ed296fc022abddf696dd9b5a472e486
serendipity019 <navagio09@gmail.com>
2025-07-29 12:16:17 +0300

    feat: i create example of administrative data for Greece

---
+++ frontend/srs/data/administrative-data.ts
@@ -0,0 +1,11 @@
+
+export const regionData = {
+    Attiki: {
+        "Athens Central": ["Athens", "Nea Philadelphia", "Zografou", "Vyronas", "Galatsi", "Kaisariani", "Dafni", "Ymittos", "Nea Chalkidon", "Ilioupoli"],
+        "Piraeus": ["Piraeus", "Keratsini", "Drapetsona", "Perama", "Korydallos", "Agios Ioannis Rentis"],    
+    },
+    "Central Macedonia": {
+        "Thessaloniki": ["Thessaloniki", "Kalamaria", "Neapoli", "Pilea", "Pylaia", "Thermi", "Halkidona", "Evosmos", "Menemeni", "Ampelokipoi", "Thermaikos", "Delta", "Oraiokastro","Langadas", ],
+        "Serres": ["Serres", "Sintiki", "Nea Zichni", "Visaltia", "Irakleia", "Emmanouil Pappas", "Amphipolis"],
+    },
+    }
