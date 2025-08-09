0a8b95d2618aeb4a006a8503161ec5db2005adb9
serendipity019 <navagio09@gmail.com>
2025-08-09 18:03:09 +0300

    feat: i create request Schema

---
+++ frontend/src/types/RequestSchema.ts
@@ -0,0 +1,22 @@
+import {z} from "zod"; 
+import { contactForm } from "./ContactSchema";
+
+export const requestForm = z.object({
+    region: z.string().min(1, "Region is required"),
+    county: z.string().min(1, "County is required"),
+    area: z.string().min(1, "Area is required"),
+    priceFrom: z.coerce.number().positive(),
+    priceTo: z.coerce.number().positive(),
+    category:z.string().min(1, "Category is required"),
+    type: z.string().min(1, "Property type is required"),
+    status: z.enum(["Rent", "Buy"]).default("Rent"),
+    squareMetersFrom: z.coerce.number().positive(),
+    squareMetersTo: z.coerce.number().positive(),
+    description: z.string().min(7)
+});
+
+export const requestSchema = requestForm.merge(contactForm);
+
+export type requestPropertyType = z.infer<typeof requestForm>;
+
+export type requestOnlyType = z.infer<typeof requestForm>;
