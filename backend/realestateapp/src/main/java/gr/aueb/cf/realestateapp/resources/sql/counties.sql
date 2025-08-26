09dff7ef0aa653ecd3f41e4163f34f1d24cac103
serendipity019 <navagio09@gmail.com>
2025-08-26 23:28:46 +0300

    feat: I create sql files with some initial data.

---
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/resources/sql/counties.sql
@@ -0,0 +1,7 @@
+INSERT INTO counties (id, name, region_id) VALUES
+(1,'Athens Central', 1),
+(2,'Piraeus', 1),
+(3,'Thessaloniki', 2),
+(4,'Serres', 2),
+ALTER TABLE counties AUTO_INCREMENT = 5;
+
---
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/resources/sql/regions.sql
@@ -0,0 +1,4 @@
+INSERT INTO regions (id, name) VALUES
+(1,'Attiki'),
+(2,'Central Macedonia')
+ALTER TABLE regions AUTO_INCREMENT = 3;
