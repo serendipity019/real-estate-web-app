1bff5f13d4f7a04f85eda93158917a170e731c92
serendipity019 <navagio09@gmail.com>
2025-08-28 14:03:16 +0300

    feat: I created Admin Initializer to pass initial admin in database.

---
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/model/config/AdminInitializer.java
@@ -0,0 +1,40 @@
+package gr.aueb.cf.realestateapp.model.config;
+
+import gr.aueb.cf.realestateapp.core.enums.ContactHours;
+import gr.aueb.cf.realestateapp.core.enums.RoleEnum;
+import gr.aueb.cf.realestateapp.model.UserEntity;
+import gr.aueb.cf.realestateapp.model.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AdminInitializer {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    CommandLineRunner adminInitDB(UserRepository userRepository){
+        return args -> {
+            String adminEmail = "admin@realestate.com";
+
+            if(userRepository.findByEmail(adminEmail).isEmpty()) {
+                UserEntity admin = new UserEntity();
+                admin.setName("Admin");
+                admin.setSurname("Admin");
+                admin.setEmail(adminEmail);
+                admin.setPhone("2104578126");
+                admin.setContactHours(ContactHours.ALL_DAY);
+                admin.setRole(RoleEnum.ADMIN);
+                admin.setPassword(passwordEncoder().encode("admin1234"));
+
+                userRepository.save(admin);
+                System.out.println("Default admin created with email: " + adminEmail);
+            }
+        };
+    }
+}
