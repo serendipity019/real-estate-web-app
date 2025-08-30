ab61521c23095fe0a4e042b565751e778c4bbd15
serendipity019 <navagio09@gmail.com>
2025-08-30 20:13:02 +0300

    feat: Add 3 new status.

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/core/enums/PropertyStatusEnum.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/core/enums/PropertyStatusEnum.java
@@ -1,8 +1,11 @@
 package gr.aueb.cf.realestateapp.core.enums;
 
 public enum PropertyStatusEnum {
-    AVAILABLE,
-    RESERVED,
-    SOLD,
-    RENTED
+    UNOBSERVED, // The default price
+    AVAILABLE, // For the Assigns
+    RESERVED, // For the Assigns
+    SOLD, // For the Assigns
+    RENTED, // For the Assigns
+    MATCHED, // For the requesting
+    UNMATCHED // For the requesting
 }
