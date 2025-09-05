bc3e3b0147c251d96d4ac87eb13642c8a476ab1d
serendipity019 <navagio09@gmail.com>
2025-09-05 23:35:35 +0300

    fix: a typo problem fixed

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,20 +1,20 @@
-import Header from './Header';
+import Header from './HeaderEn';
 import Footer from './Footer';
 import type { ReactNode } from 'react';
 
 
 const MainLayout = ({children}: {children: ReactNode}) => {
   return (
-    <>
-    <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-            {children}
-        </main>
-        <Footer />
-    </div>  
-    </>    
-        );
+        <>
+            <div className="flex flex-col min-h-screen">
+                <Header />
+                <main className="flex-grow container mx-auto px-4 py-8">
+                    {children}
+                </main>
+                <Footer />
+            </div>  
+        </>    
+    );
 };
 
 export default MainLayout;
