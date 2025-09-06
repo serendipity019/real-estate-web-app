0992fc5f8b99c4220f8d7a439a9a55af09abf1ac
serendipity019 <navagio09@gmail.com>
2025-09-06 23:57:12 +0300

    feat: i created insert DTO for Assign properties

--- frontend/src/types/dto/AssignPropertyDTO.ts
+++ frontend/src/types/dto/AssignPropertyDTO.ts
@@ -15,11 +15,3 @@ export interface AssignPropertyDTO {
     postCode?: string;
 };
 
-export interface UsernameDTO {
-    name: string;
-    surname: string;
-    email: string;
-    phoneNumber: string;
-    contactHours: "All day" | "Morning" | "Afternoon";
-    password?: string;
-};
