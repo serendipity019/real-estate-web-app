219660ddb75813d6a4b93be8e0380657a626d80a
serendipity019 <navagio09@gmail.com>
2025-09-07 23:12:11 +0300

    feat: I created sql file to drop all the tables in the MySql DB.

---
+++ backend/realestateapp/src/main/resources/static/sql/dropTables.sql
@@ -0,0 +1,8 @@
+Drop Table assign_properties;
+Drop Table request_properties;
+Drop Table areas;
+Drop Table counties;
+Drop Table regions;
+Drop Table property_types;
+Drop Table property_categories;
+Drop Table users;
