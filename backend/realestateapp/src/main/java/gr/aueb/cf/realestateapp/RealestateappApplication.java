c7a40f3c008c7af25eff757951476d7c7a5fe5ba
serendipity019 <navagio09@gmail.com>
2025-09-07 23:10:22 +0300

    update: I created @EnableJpaREpositories to know from where to take e.g. the sql data.

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/RealestateappApplication.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/RealestateappApplication.java
@@ -3,9 +3,11 @@ package gr.aueb.cf.realestateapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "gr.aueb.cf.realestateapp.repository")
 public class RealestateappApplication {
 
 	public static void main(String[] args) {
