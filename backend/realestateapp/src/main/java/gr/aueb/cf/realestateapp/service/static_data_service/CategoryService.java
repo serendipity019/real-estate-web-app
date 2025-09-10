e1defcb473eaed615fe9847c1c2fa4898b97e325
serendipity019 <navagio09@gmail.com>
2025-09-11 00:00:02 +0300

    update: i added method getCategoryById

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/service/static_data_service/CategoryService.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/service/static_data_service/CategoryService.java
@@ -1,11 +1,14 @@
 package gr.aueb.cf.realestateapp.service.static_data_service;
 
+import gr.aueb.cf.realestateapp.core.exceptions.AppObjectNotFoundException;
 import gr.aueb.cf.realestateapp.dto.static_dto.PropertyCategoryResponseDTO;
+import gr.aueb.cf.realestateapp.model.static_data.PropertyCategoriesEntity;
 import gr.aueb.cf.realestateapp.repository.static_repo.PropertyCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,5 +22,14 @@ public class CategoryService {
                 .map(category -> new PropertyCategoryResponseDTO(
                         category.getId(), category.getName()))
                 .collect(Collectors.toList());
+    }
+
+    public Optional<PropertyCategoriesEntity> getCategoryById(Long id) throws AppObjectNotFoundException {
+        Optional<PropertyCategoriesEntity> category = categoryRepository.findById(id);
+
+        if (category.isEmpty()) {
+            throw new AppObjectNotFoundException("", "No category found with id " + id);
+        }
+        return category;
     }
 }
