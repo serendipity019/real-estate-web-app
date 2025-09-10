fc17b26bd0079358b8d8ce2ae44bf099ae7c59b3
serendipity019 <navagio09@gmail.com>
2025-09-10 23:58:29 +0300

    fix: I made correct tests for bad and good scenarios.

--- backend/realestateapp/src/test/java/gr/aueb/cf/realestateapp/static_data/restApi/CountyControllerIT.java
+++ backend/realestateapp/src/test/java/gr/aueb/cf/realestateapp/static_data/restApi/CountyControllerIT.java
@@ -1,40 +1,44 @@
 package gr.aueb.cf.realestateapp.static_data.restApi;
 
-import gr.aueb.cf.realestateapp.core.exceptions.AppObjectNotFoundException;
-import gr.aueb.cf.realestateapp.core.exceptions.AppServerException;
+import gr.aueb.cf.realestateapp.RealestateappApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(classes = RealestateappApplication.class)
 @AutoConfigureMockMvc
 public class CountyControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @Rollback
     void shouldReturnCountiesByRegionId() throws Exception {
-        mockMvc.perform(get("/api/counties")
-                .param("regionId", "1"))
+        mockMvc.perform(get("/api/counties/1")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").exists())
-                .andExpect(jsonPath("$[0].name").value("Athens Central"));
+                .andExpect(jsonPath("$[1].id").exists())
+                .andExpect(jsonPath("$[1].name").exists());
     }
 
     @Test
-    @Rollback
     void shouldReturnNotFoundException() throws Exception {
-        mockMvc.perform(get("/api/counties")
-                .param("regionId", "5"))
+        mockMvc.perform(get("/api/counties/999")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.code").value("COUNTY_NOT_FOUND"))
-                .andExpect(jsonPath("$.message").value("No counties found for region with id 5"));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value("NotFound"));
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenRegionIdIsInvalid() throws Exception {
+        mockMvc.perform(get("/api/counties/abc")  // invalid number
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
