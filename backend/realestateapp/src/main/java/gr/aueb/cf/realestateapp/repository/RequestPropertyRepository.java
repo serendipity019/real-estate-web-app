8f205a6384531728b015cab6d27b27c6a3ae86c3
serendipity019 <navagio09@gmail.com>
2025-09-11 00:03:14 +0300

    update: add new methods

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/RequestPropertyRepository.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/RequestPropertyRepository.java
@@ -3,6 +3,9 @@ package gr.aueb.cf.realestateapp.repository;
 import gr.aueb.cf.realestateapp.core.enums.PropertyStatusEnum;
 import gr.aueb.cf.realestateapp.model.AssignPropertyEntity;
 import gr.aueb.cf.realestateapp.model.RequestPropertyEntity;
+import gr.aueb.cf.realestateapp.model.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -13,6 +16,7 @@ public interface RequestPropertyRepository extends JpaRepository<RequestProperty
         JpaSpecificationExecutor<RequestPropertyEntity> {
 
     Optional<RequestPropertyEntity> findByUuid(String uuid);
-    List<RequestPropertyEntity> findByUserId(Long userId);
+    List<RequestPropertyEntity> findByUser(UserEntity user);
     List<RequestPropertyEntity> findByRealEstateStatus(PropertyStatusEnum statusEnum);
+    Page<RequestPropertyEntity> findByRealEstateStatus(PropertyStatusEnum statusEnum, Pageable pageable);
 }
