562c389380410f99c561d2366f5e473771984426
serendipity019 <navagio09@gmail.com>
2025-09-12 01:51:53 +0300

    update: add new methods for implementation.

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/UserRepository.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package gr.aueb.cf.realestateapp.repository;
 
+import gr.aueb.cf.realestateapp.core.enums.RoleEnum;
 import gr.aueb.cf.realestateapp.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,6 +14,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long>,
         JpaSpecificationExecutor<UserEntity> {
 
     Optional<UserEntity> findByEmail(String email);
-
+    Optional<UserEntity> findById(Long id);
+    List<UserEntity> findByRole(RoleEnum role);
     boolean existsByEmail(String email);
+    boolean isAdminOrAgent(RoleEnum role);
+    boolean isAdmin(RoleEnum role);
 }
