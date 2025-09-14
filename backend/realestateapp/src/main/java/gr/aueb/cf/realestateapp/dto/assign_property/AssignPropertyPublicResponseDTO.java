8f364f007907a93cdd4b1b75d5cbed94b4ceb39e
serendipity019 <navagio09@gmail.com>
2025-09-14 17:56:30 +0300

    fix: major typo where create big confuse. Add AssignTypeEnum

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/dto/assign_property/AssignPropertyPublicResponseDTO.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/dto/assign_property/AssignPropertyPublicResponseDTO.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.realestateapp.dto.assign_property;
 
+import gr.aueb.cf.realestateapp.core.enums.AssignTypeEnum;
+
 import java.time.LocalDateTime;
 
 public record AssignPropertyPublicResponseDTO(
@@ -9,9 +11,14 @@ public record AssignPropertyPublicResponseDTO(
         String area,
         String category,
         String type,
+        AssignTypeEnum assignTypeEnum,
         String description,
         Integer price,
         Integer squareMeters,
+        String street,
+        String streetNumber,
+        Long postCode,
+        LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
 }
