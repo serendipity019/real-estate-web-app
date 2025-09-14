8bb95feeaa487c99bcf8335ed15bc31daa6a2327
serendipity019 <navagio09@gmail.com>
2025-09-14 17:57:50 +0300

    fix: rename to RealEstateStatusEnum the AssignStatusEnum

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/AssignPropertyRepository.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/repository/AssignPropertyRepository.java
@@ -1,7 +1,11 @@
 package gr.aueb.cf.realestateapp.repository;
 
-import gr.aueb.cf.realestateapp.core.enums.PropertyStatusEnum;
+import gr.aueb.cf.realestateapp.core.enums.AssignTypeEnum;
+import gr.aueb.cf.realestateapp.core.enums.RealEstateStatusEnum;
 import gr.aueb.cf.realestateapp.model.AssignPropertyEntity;
+import gr.aueb.cf.realestateapp.model.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -12,6 +16,11 @@ public interface AssignPropertyRepository extends JpaRepository<AssignPropertyEn
         JpaSpecificationExecutor<AssignPropertyEntity> {
 
         Optional<AssignPropertyEntity> findByUuid(String uuid);
-        List<AssignPropertyEntity> findByUserId(Long userId);
-        List<AssignPropertyEntity> findByRealEstateStatus(PropertyStatusEnum statusEnum);
+        List<AssignPropertyEntity> findByUser(UserEntity user);
+        List<AssignPropertyEntity> findAll();
+        Page<AssignPropertyEntity> findAll(Pageable pageable);
+        List<AssignPropertyEntity> findByRealEstateStatus(RealEstateStatusEnum statusEnum);
+        Page<AssignPropertyEntity> findByRealEstateStatus(RealEstateStatusEnum statusEnum, Pageable pageable);
+        List<AssignPropertyEntity> findByRequestPurpose(AssignTypeEnum typeEnum);
+        Page<AssignPropertyEntity> findByRequestPurpose(AssignTypeEnum typeEnum, Pageable pageable);
 }
