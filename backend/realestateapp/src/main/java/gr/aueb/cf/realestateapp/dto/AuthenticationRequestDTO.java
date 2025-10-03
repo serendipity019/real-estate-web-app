0f9940efc9c741623d2af4b8ea9481b561f4b123
serendipity019 <navagio09@gmail.com>
2025-10-03 12:20:15 +0300

    update minor: for invalid email

--- backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/dto/AuthenticationRequestDTO.java
+++ backend/realestateapp/src/main/java/gr/aueb/cf/realestateapp/dto/AuthenticationRequestDTO.java
@@ -1,11 +1,13 @@
 package gr.aueb.cf.realestateapp.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record AuthenticationRequestDTO(
         @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
         String email,
 
         @NotBlank(message = "Password is required")
